2eac63197fb597a371cfabd669b86cfcf323f880
XiangXing <992348156@qq.com>
2024-06-23 20:03:54 +0800

    Create merge.py

---
+++ 1_initial_dataset/1_aimd/2_read_aimd_and_extract/merge.py
@@ -0,0 +1,78 @@
+"""
+Select structures
+===========================
+
+merge structures from aimd and extract some structures every 50 structures
+"""
+from ase.io import read, write
+import numpy as np
+import matplotlib.pyplot as plt
+from ase import Atoms
+import copy
+from ase.md import analysis as traj_analysis
+
+
+# Read OUTCAR
+fcc = []
+folder = "/media/mee2123/a1d48093-011c-4506-9626-2551684cff1e/data/Li3InCl6/rough_database/1_aimd/200"
+file = folder + "/OUTCAR"
+print(file)
+temp = read(file, ":")
+for j in temp:
+	fcc.append(j)
+write('aimd_200.traj', [i for i in temp])
+
+
+folder = "/media/mee2123/a1d48093-011c-4506-9626-2551684cff1e/data/Li3InCl6/rough_database/1_aimd/400"
+file = folder + "/OUTCAR"
+print(file)
+temp = read(file, ":")
+for j in temp:
+	fcc.append(j)
+write('aimd_400.traj', [i for i in temp])
+
+folder = "/media/mee2123/a1d48093-011c-4506-9626-2551684cff1e/data/Li3InCl6/rough_database/1_aimd/600"
+file = folder + "/OUTCAR"
+print(file)
+temp = read(file, ":")
+for j in temp:
+	fcc.append(j)
+write('aimd_600.traj', [i for i in temp])
+
+folder = "/media/mee2123/a1d48093-011c-4506-9626-2551684cff1e/data/Li3InCl6/rough_database/1_aimd/600/go_on"
+file = folder + "/OUTCAR"
+print(file)
+temp = read(file, ":")
+for j in temp:
+	fcc.append(j)
+write('aimd_600_go_on.traj', [i for i in temp])
+
+folder = "/media/mee2123/a1d48093-011c-4506-9626-2551684cff1e/data/Li3InCl6/rough_database/1_aimd/800"
+file = folder + "/OUTCAR"
+print(file)
+temp = read(file, ":")
+for j in temp:
+	fcc.append(j)
+write('aimd_800.traj', [i for i in temp])
+
+folder = "/media/mee2123/a1d48093-011c-4506-9626-2551684cff1e/data/Li3InCl6/rough_database/1_aimd/1000"
+file = folder + "/OUTCAR"
+print(file)
+temp = read(file, ":")
+for j in temp:
+	fcc.append(j)
+write('aimd_1000.traj', [i for i in temp])
+
+print("Number of fcc:")
+print(len(fcc))
+
+all = []
+for i in fcc:
+	all.append(i)
+
+all_reduced_50 = []
+for i in range(0, len(fcc), 50):
+	all_reduced_50.append(fcc[i])
+
+write('all.traj', [i for i in all])
+write('all_reduced_for_initial.traj', [i for i in all_reduced_50])
